6f1a631916a8d6d18937477ded8be8fa79204ff6
bradosia <bradosia@gmail.com>
2020-12-02 23:09:36 -0800

    port over

---
+++ src/core/config.hpp
@@ -0,0 +1,14 @@
+/*
+ * @name Bookfiler™ Recognize Module
+ * @author Branden Lee
+ * @version 1.00
+ * @license GNU LGPL v3
+ * @brief text recognition.
+ */
+
+#ifndef BOOKFILER_LIB_IMAGE_TO_HOCR_CONFIG_H
+#define BOOKFILER_LIB_IMAGE_TO_HOCR_CONFIG_H
+
+#define BOOKFILER_MODULE_OCR_DEBUG 1
+
+#endif // BOOKFILER_LIB_IMAGE_TO_HOCR_CONFIG_H
---
+++ src_example/example00/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * @name BookFiler Library - Sort Filter Tree Widget
+ * @author Branden Lee
+ * @version 1.00
+ * @license MIT
+ * @brief Super fast tree sorting and filtering tree widget.
+ */
+
+// C++
+#include <iostream>
+#include <vector>
+
+// Bookfiler Libraries
+#include "../../src/core/Ocr.hpp"
+#include <BookFiler-Lib-image-to-hOCR/Interface.hpp>
+
+std::string testName = "Sort Filter Tree Widget Example 00";
+
+int main(int argc, char *argv[]) {
+  std::cout << testName << " BEGIN" << std::endl;
+
+
+
+  std::cout << testName << " END" << std::endl;
+  return 0;
+}
